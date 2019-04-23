9ecfd44c582de5a109c5697d4453b6d1c3deb4c3
Miguel Ángel Urbina <miguel.urbina15.1997@gmail.com>
2019-04-23 09:28:31 -0500

    Aplicaciones a vacantes addded

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing_page#index'
   resources :accidentes_laborales
+  resources :aplicaciones_a_vacantes
   resources :areas
   resources :areas_por_empleado
   resources :asistencias
