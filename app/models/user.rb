4269f481bc0cd778bd26e4028049b2d082c0d3f9
Miguel Ángel Urbina <miguel.urbina15.1997@gmail.com>
2019-04-24 10:55:09 -0500

    User can select the role when sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :roles
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable
 
+         enum roles: %i[admin hotel restaurant rrhh ]
 
 end
